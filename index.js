c885974726d09d53e2d4add867d9c4728fba42ea
Alex <AlexCarterDeveloper@gmail.com>
2019-01-09 23:28:56 +0300

    Static server, apps

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`)
